1fb2beab7a058dfda978c0b81c7bd55f874ba94b
BVBFD <lsevina126@gmail.com>
2022-06-16 14:12:06 +0900

    fixing error that game drag location is not correct on dragging

--- src/pages/DragGamePracticePage/DragGamePracticePage.jsx
+++ src/pages/DragGamePracticePage/DragGamePracticePage.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 // import DragGamePractice from '../../DragGamePractice/src/Main'
-import { config } from '../../DragGamePractice/src/Main'
-import { IonPhaser } from '@ion-phaser/react'
-import styles from './DragGamePracticePage.module.css'
+import { config } from '../../DragGamePractice/src/Main';
+import { IonPhaser } from '@ion-phaser/react';
+import styles from './DragGamePracticePage.module.css';
 
 const DragGamePracticePage = (props) => {
   return (
     <div className={styles.IonPhaserBox}>
       <IonPhaser game={config} initialize={true} />
     </div>
-  )
-}
+  );
+};
 
-export default DragGamePracticePage
+export default DragGamePracticePage;
